de8558901be92a774cf0b84c2a1e2edbdb882954
Kevin Wei <chenghaowei@gmail.com>
2019-10-23 13:53:13 -0400

    add skipped smoke test

--- src/tests/ItemDetail.test.js
+++ src/tests/ItemDetail.test.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ItemDetail from '../components/ItemDetail';
 import { ItemProvider } from '../ItemContext';
+import { MemoryRouter } from 'react-router-dom';
 
-describe.skip('<ItemDetail />', () => {
+describe('<ItemDetail />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(
       <ItemProvider>
-        <ItemDetail />
+        <ItemDetail
+          match={{ params: { item_id: '00000000-0000-0000-0000-000000000000' } }}
+        />
       </ItemProvider>,
       div
     );
